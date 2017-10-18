81b85fda099a8105e10b8eaec749d42273d1d62b
florian Burel <florian.burel@mac.com>
2017-10-18 16:51:04 +0200

    Started a new bitManipulation package

---
+++ app/src/main/java/f10/net/androidtoolbox/bitManipulation/BitHelper.java
@@ -0,0 +1,20 @@
+package f10.net.androidtoolbox.bitManipulation;
+
+/**
+ * Created by fl0 on 18/10/2017.
+ */
+
+public class BitHelper {
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+}
